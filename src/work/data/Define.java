bad9c1dd105e7318e86b471639f8f35d0f8d9488
hago <hago89@naver.com>
2017-03-17 11:13:48 +0900

    조건검색 항목 추가(카페)

--- src/work/data/Define.java
+++ src/work/data/Define.java
@@ -14,7 +14,7 @@ public class Define {
 	public static final double COORD_DONGBU_LNG = 127.057700;
 	public static final String[] COORDS_DONGBU = {"37.5093347", "127.057700"}; 
 	
-	public static final String[] ARY_MENUS = {"한식", "일식", "분식", "중식", "양식", "아시안", "술집", "퓨전음식", "치킨", "족발/보쌈", "피자/버거"};
+	public static final String[] ARY_MENUS = {"한식", "일식", "분식", "중식", "양식", "아시안", "술집", "퓨전음식", "치킨", "족발/보쌈", "피자/버거", "카페"};
 	public static final String[] ARY_PRICES = {"1", "2", "3"};
 	public static final String[] ARY_DISTANCES = {"1", "3", "5", "10"};
 	public static final String[] ARY_RATES = {"1", "2", "3", "4", "5"};
